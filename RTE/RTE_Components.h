1e7ee87b9af7a5a6ec569ac3fd978f55d9784922
Jithin <jithin@finrobotics.com>
2015-08-17 15:38:26 +0530

    [WIP] Give structure

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'rfduino_nordic_ble' 
+ * Target:  'nRF51822' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define S110
+#define SOFTDEVICE_PRESENT
+#define SWI_DISABLE0
+
+#endif /* RTE_COMPONENTS_H */
